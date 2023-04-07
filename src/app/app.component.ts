21644508bfe9f4300f71931b5650954241ccf621
edupoly <info.edupoly@gmail.com>
2023-04-07 09:22:33 +0530

    directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ang20app';
+  mynumber=19282;
+  students = [
+    {
+      name:'prakruthi',
+      gender:'female',
+      age:34
+    },
+    {
+      name:'jayaram',
+      gender:'male',
+      age:54
+    },
+    {
+      name:'hima',
+      gender:'female',
+      age:14
+    },
+    {
+      name:'gauri',
+      gender:'female',
+      age:24
+    },
+    {
+      name:'naman',
+      gender:'male',
+      age:22
+    },
+]
 }
