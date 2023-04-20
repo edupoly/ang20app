39285a648ce10dfe872fa01a9c045bb76ef5d575
edupoly <info.edupoly@gmail.com>
2023-04-20 08:25:50 +0530

    rounting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AbcdComponent } from './abcd/abcd.component';
+import { HomeComponent } from './home/home.component';
+import { XyzComponent } from './xyz/xyz.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'abcd',
+    component:AbcdComponent
+  },
+  {
+    path:"xyz",
+    component:XyzComponent
+  },
+  {
+    path:"",
+    component:HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AbcdComponent } from './abcd/abcd.component';
+import { XyzComponent } from './xyz/xyz.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AbcdComponent,
+    XyzComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
